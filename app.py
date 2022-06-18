80ab181c1699b0c78fc0656ddaaf3585576ef082
pavankumar kosaraju <pavankumarkosaraju@gmail.com>
2022-06-18 14:34:09 +0530

    Added the logger and Expection module

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask
+from housing.logger import logging
+from housing.exception import HousingException
+import sys
 
-app = Flask(__name__)
+app=Flask(__name__)
 
-@app.route('/',methods=['GET','POST'])
+
+@app.route("/",methods=['GET','POST'])
 def index():
-    return 'CI/CD pipeline is established'
+    try:
+        raise Exception("We are testing custom exception")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("We are testing logging module")
+    return "CI CD pipeline has been established."
 
-# Run the app
-if __name__ == '__main__':
+
+if __name__=="__main__":
     app.run(debug=True)
